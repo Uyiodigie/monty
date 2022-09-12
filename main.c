95159b6532a741dafe552812d2c966313871cee4
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-09-11 18:22:27 -0700

    test

--- main.c
+++ main.c
@@ -1,103 +1,50 @@
 #include "monty.h"
 
-global_t vglo;
+vars gvars;
 
 /**
- * free_vglo - frees the global variables
- *
- * Return: no return
- */
-void free_vglo(void)
-{
-	free_dlistint(vglo.head);
-	free(vglo.buffer);
-	fclose(vglo.fd);
-}
-
-/**
- * start_vglo - initializes the global variables
- *
- * @fd: file descriptor
- * Return: no return
- */
-void start_vglo(FILE *fd)
-{
-	vglo.lifo = 1;
-	vglo.cont = 1;
-	vglo.arg = NULL;
-	vglo.head = NULL;
-	vglo.fd = fd;
-	vglo.buffer = NULL;
-}
-
-/**
- * check_input - checks if the file exists and if the file can
- * be opened
- *
+ * main - runs monty interpreter
  * @argc: argument count
  * @argv: argument vector
- * Return: file struct
+ * Return: return -1 if failed, else 1
  */
-FILE *check_input(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	FILE *fd;
+	FILE *fp;
+	stack_t *head = NULL;
+	ssize_t read = 0;
+	size_t len = 0;
+	int token_ret = 0;
 
-	if (argc == 1 || argc > 2)
+	gvars.lineptr = NULL;
+	gvars.line_number = 0;
+	if (argc != 2)
 	{
-		dprintf(2, "USAGE: monty file\n");
+		fprintf(stdout, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
-	fd = fopen(argv[1], "r");
-
-	if (fd == NULL)
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
 	{
-		dprintf(2, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stdout, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
-	return (fd);
-}
-
-/**
- * main - Entry point
- *
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success
- */
-int main(int argc, char *argv[])
-{
-	void (*f)(stack_t **stack, unsigned int line_number);
-	FILE *fd;
-	size_t size = 256;
-	ssize_t nlines = 0;
-	char *lines[2] = {NULL, NULL};
-
-	fd = check_input(argc, argv);
-	start_vglo(fd);
-	nlines = getline(&vglo.buffer, &size, fd);
-	while (nlines != -1)
+	while ((read = getline(&gvars.lineptr, &len, fp)) != -1)
 	{
-		lines[0] = _strtoky(vglo.buffer, " \t\n");
-		if (lines[0] && lines[0][0] != '#')
+		gvars.line_number++;
+		token_ret = tokenizer(gvars.lineptr);
+		if (token_ret != -1)
 		{
-			f = get_opcodes(lines[0]);
-			if (!f)
+			find_op(&head);
+			if (gvars.ret_val == -1)
 			{
-				dprintf(2, "L%u: ", vglo.cont);
-				dprintf(2, "unknown instruction %s\n", lines[0]);
-				free_vglo();
+				free(gvars.lineptr);
+				free_l(head);
+				fclose(fp);
 				exit(EXIT_FAILURE);
 			}
-			vglo.arg = _strtoky(NULL, " \t\n");
-			f(&vglo.head, vglo.cont);
 		}
-		nlines = getline(&vglo.buffer, &size, fd);
-		vglo.cont++;
 	}
-
-	free_vglo();
-
-	return (0);
+	free(gvars.lineptr); free_l(head); fclose(fp);
+	return (gvars.ret_val);
 }
